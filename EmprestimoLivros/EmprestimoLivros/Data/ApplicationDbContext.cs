f9825425e75a6f32c7d392b819dc09d1c63d4ff7
ThIaGoOLuiZz <thiagoluizgon13@gmail.com>
2024-03-22 21:52:42 -0300

    Iniciado construção do projeto EmprestimoLivros

---
+++ EmprestimoLivros/EmprestimoLivros/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using EmprestimoLivros.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmprestimoLivros.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<EmprestimoModel> Emprestimos { get; set; }
+    }
+}
---
+++ EmprestimoLivros/EmprestimoLivros/Models/EmprestimoModel.cs
@@ -0,0 +1,11 @@
+﻿namespace EmprestimoLivros.Models
+{
+    public class EmprestimoModel
+    {
+        public int id { get; set; }
+        public string Recebedor { get; set; }
+        public string Fornecedor { get; set; }
+        public string LivroEmprestado { get; set; }
+        public DateTime dataUltimaAtualizacao { get; set; } = DateTime.Now;
+    }
+}
