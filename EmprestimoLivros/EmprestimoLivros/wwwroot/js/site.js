4698b4ae849232d9db3b948a18befe85296dbcb5
ThIaGoOLuiZz <thiagoluizgon13@gmail.com>
2024-04-12 22:05:21 -0300

    Adicionado filtros para a tabela de emprestimos

--- EmprestimoLivros/EmprestimoLivros/wwwroot/js/site.js
+++ EmprestimoLivros/EmprestimoLivros/wwwroot/js/site.js
@@ -1,4 +1,29 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿$(document).ready(function () {
+    $('#Emprestimos').DataTable({
+        language: 
+            {
+            "decimal": "",
+            "emptyTable": "No data available in table",
+            "info": "Mostrando _START_ registro de _END_ em um total de _TOTAL_ entradas",
+            "infoEmpty": "Showing 0 to 0 of 0 entries",
+            "infoFiltered": "(filtered from _MAX_ total entries)",
+            "infoPostFix": "",
+            "thousands": ",",
+            "lengthMenu": "Mostrar _MENU_ entradas",
+            "loadingRecords": "Loading...",
+            "processing": "",
+            "search": "Procurar:",
+            "zeroRecords": "No matching records found",
+            "paginate": {
+                "first": "Primeiro",
+                "last": "Último",
+                "next": "Próximo",
+                "previous": "Anterior"
+            },
+            "aria": {
+                "orderable": "Order by this column",
+                "orderableReverse": "Reverse order this column"
+            }
+        }
+    });
+})
