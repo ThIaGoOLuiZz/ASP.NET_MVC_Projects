dc2f4e14743dcd55d676445c9e73c77226477917
ThIaGoOLuiZz <thiagoluizgon13@gmail.com>
2024-04-15 23:30:54 -0300

    Adicionado função de exportar planilha em excel

--- EmprestimoLivros/EmprestimoLivros/Controllers/EmprestimoController.cs
+++ EmprestimoLivros/EmprestimoLivros/Controllers/EmprestimoController.cs
@@ -1,6 +1,8 @@
-﻿using EmprestimoLivros.Data;
+﻿using ClosedXML.Excel;
+using EmprestimoLivros.Data;
 using EmprestimoLivros.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Data;
 
 namespace EmprestimoLivros.Controllers
 {
@@ -62,11 +64,54 @@ namespace EmprestimoLivros.Controllers
             return View(emprestimo);
         }
 
+        public IActionResult Exportar()
+        {
+            var dados = GetDados();
+
+            using (XLWorkbook workbook = new XLWorkbook())
+            {
+                workbook.AddWorksheet(dados,"Dados empréstimo");
+
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    workbook.SaveAs(ms);
+                    return File(ms.ToArray(), "application/vnd.openxmlformats-officedocument.spreadsheet.sheet", "Emprestimo.xls");
+                }
+            }
+        }
+
+        private DataTable GetDados()
+        {
+            DataTable dataTable = new DataTable();
+
+            dataTable.TableName = "Dados empréstimos";
+
+            dataTable.Columns.Add("Recebedor", typeof(string));
+            dataTable.Columns.Add("Fornecedor", typeof(string));
+            dataTable.Columns.Add("Livro", typeof (string));
+            dataTable.Columns.Add("Data de empréstimo", typeof(DateTime));
+
+            var dados = _db.Emprestimos.ToList();
+
+            if(dados.Count > 0)
+            {
+                dados.ForEach(emprestimo =>
+                {
+                    dataTable.Rows.Add(emprestimo.Recebedor, emprestimo.Fornecedor, emprestimo.LivroEmprestado, emprestimo.dataUltimaAtualizacao);
+                });
+            }
+
+            return dataTable;
+        }
+
+
         [HttpPost]
         public IActionResult Cadastrar(EmprestimoModel emprestimos)
         {
             if(ModelState.IsValid)
             {
+                emprestimos.dataUltimaAtualizacao = DateTime.Now;
+
                 _db.Emprestimos.Add(emprestimos);
                 _db.SaveChanges();
 
@@ -82,7 +127,13 @@ namespace EmprestimoLivros.Controllers
         {
             if(ModelState.IsValid)
             {
-                _db.Emprestimos.Update(emprestimo);
+                var emprestimoDB = _db.Emprestimos.Find(emprestimo.id);
+
+                emprestimoDB.Fornecedor = emprestimo.Fornecedor;
+                emprestimoDB.Recebedor = emprestimo.Recebedor;
+                emprestimoDB.LivroEmprestado = emprestimo.LivroEmprestado;
+
+                _db.Emprestimos.Update(emprestimoDB);
                 _db.SaveChanges();
 
                 TempData["MensagemSucesso"] = "Edição realizada com sucesso!";
--- EmprestimoLivros/EmprestimoLivros/Models/EmprestimoModel.cs
+++ EmprestimoLivros/EmprestimoLivros/Models/EmprestimoModel.cs
@@ -14,6 +14,6 @@ namespace EmprestimoLivros.Models
 
         [Required(ErrorMessage = "Digite o nome do Livro!")]
         public string LivroEmprestado { get; set; }
-        public DateTime dataUltimaAtualizacao { get; set; } = DateTime.Now;
+        public DateTime dataUltimaAtualizacao { get; set; }
     }
 }
