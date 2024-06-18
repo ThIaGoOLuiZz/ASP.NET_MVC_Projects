fc55c599e68e57d01bea1c5c2e8d6f1c46f38abc
ThIaGoOLuiZz <thiagoluizgon13@gmail.com>
2024-06-18 19:42:52 -0300

    Adicionado novo projeto: VMController. Inserido string de conexão

---
+++ VMController/VMController/Data/ContextoUsuario.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection.Emit;
+using VMController.Models;
+
+namespace VMController.Data
+{
+    public class ContextoUsuario : DbContext
+    {
+        public ContextoUsuario(DbContextOptions<ContextoUsuario> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+
+        public DbSet<Usuario> Usuario { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Usuario>(entity =>
+            {
+                entity.HasKey(e => e.IdUsuario);
+            });
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ VMController/VMController/Models/Usuario.cs
@@ -0,0 +1,10 @@
+﻿namespace VMController.Models
+{
+    public class Usuario
+    {
+        public int IdUsuario { get; set; }
+        public String Nome { get; set; }
+        public String Email { get; set; }
+        public String Senha { get; set; }
+    }
+}
