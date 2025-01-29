c5abfa422e709f3f6c07826faa61f526e84aa421
Thiago Luiz <126069763+ThIaGoOLuiZz@users.noreply.github.com>
2025-01-29 11:56:23 -0300

    Delete VMController directory

--- VMController/VMController/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace VMController.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
